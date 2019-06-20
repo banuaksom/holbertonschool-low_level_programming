0b200b20eaf7fbac35bbb658841faa2c72d1a2ad
Banu Sapakova <banuaksom@gmail.com>
2019-06-20 20:09:37 +0000

    if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,16 +14,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
+
 		printf("%d is positive\n", n);
-	}
+
 	else if (n < 0)
-	{
+
 		printf("%d is negative\n", n);
-	}
+
 	else
-	{
-		printf("is zero\n");
-	}
+
+		printf("%dis zero\n", n);
+
 	return (0);
 }

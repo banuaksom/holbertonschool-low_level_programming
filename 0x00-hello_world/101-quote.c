c02ca87655ee0b18883be492e170df413ff4449d
Banu Sapakova <banuaksom@gmail.com>
2019-06-20 04:05:16 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,6 @@
  */
 int main(void)
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	while (*s)
-	putchar (*s++);
+	write(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }

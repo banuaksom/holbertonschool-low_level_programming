17709b60afebbcead274237b5a6f5a4a90649088
Banu Sapakova <banuaksom@gmail.com>
2019-07-06 23:45:03 +0000

    first check

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @lc: character to check
+ *
+ * Return: char
+ */
+
+char *string_toupper(char *s)
+{
+	char *lc = s;
+	for ( ; *s != '\0'; s++)
+	{
+		if (*s >= 'a' && *s <= 'z')
+		{
+			*s = *s - 32;
+		}
+	}
+	s = lc;
+	return (s);
+}
+

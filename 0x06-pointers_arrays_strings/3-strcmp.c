6c61faee15fa825a3582c98351e9e8a34c27eae2
Banu Sapakova <banuaksom@gmail.com>
2019-07-06 22:53:32 +0000

    third check

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,17 +8,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0')
+	while (*s1 == *s2)
 	{
 		s1++;
-	}
-	while (*s2 != '\0')
-	{
 		s2++;
+			if (*s1 == '\0')
+				return (0);
 	}
-	if (s1 < s2 || s1 > s2)
-		return (*s1 - *s2);
-	else
-		return (0);
+	return (*s1 - *s2);
 
 }

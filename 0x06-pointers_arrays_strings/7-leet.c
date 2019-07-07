865f13ff237bd87a4db7e4fe6528e3ed99ef5249
Banu Sapakova <banuaksom@gmail.com>
2019-07-07 03:17:46 +0000

    added brackets

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,16 +10,19 @@ char *leet(char *s)
 {
 	char *b = s;
 	char encode[15] = {'a', 'A', '4', 'e', 'E', '3',
-		'o', 'O', '0', 't', 'T', '7', 'l', 'L', '1'};
+		'o', 'O', '0', 't', 'T', '7', 'l', 'L', '1'
+	};
 	char *e = encode;
 
 	for ( ; *s != '\0'; s++)
 	{
 		for ( ; *e != '\0'; *e = *e + 3)
+		{
 			if (*s == *e || *s == *(e + 1))
 			{
 				*s == *(e + 2);
 			}
+		}
 	}
 	s = b;
 	return (s);

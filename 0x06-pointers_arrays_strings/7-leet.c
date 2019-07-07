45fa0956e5185ed1068c3434483d50bb4198605d
Banu Sapakova <banuaksom@gmail.com>
2019-07-07 03:12:34 +0000

    pointer to array

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * leet - encodes a string into 1337
+ * @s: character to check
+ *
+ * Return: char
+ */
+
+char *leet(char *s)
+{
+	char *b = s;
+	char encode[15] = {'a', 'A', '4', 'e', 'E', '3',
+		'o', 'O', '0', 't', 'T', '7', 'l', 'L', '1'};
+	char *e = encode;
+
+	for ( ; *s != '\0'; s++)
+	{
+		for ( ; *e != '\0'; *e = *e + 3)
+			if (*s == *e || *s == *(e + 1))
+			{
+				*s == *(e + 2);
+			}
+	}
+	s = b;
+	return (s);
+}

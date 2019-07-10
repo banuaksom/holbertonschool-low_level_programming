5896966344b026defc042fa3db9a1a9ec9ada388
Banu Sapakova <banuaksom@gmail.com>
2019-07-10 19:56:53 +0000

    first added

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: character to check
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	char *start = s;
+
+	if (*s != '\0')
+	{
+		s++;
+		_print_rev_recursion(s);
+		_putchar(*start);
+	}
+}

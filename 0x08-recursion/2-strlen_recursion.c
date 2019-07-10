85eb3071fa0553841b81675c0a19f481396c3d93
Banu Sapakova <banuaksom@gmail.com>
2019-07-10 20:21:44 +0000

    first added

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: character to chec
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s != '\0')
+	{
+		count++;
+		count += _strlen_recursion(++s);
+		return (count);
+	}
+	else
+	{
+		return (0);
+	}
+}

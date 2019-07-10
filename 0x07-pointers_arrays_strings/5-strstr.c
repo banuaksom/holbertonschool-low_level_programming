36cc6fec7d23c20df158b5db3ee2c11e1c62982d
Banu Sapakova <banuaksom@gmail.com>
2019-07-10 00:57:24 +0000

    first added

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: character to check
+ * @needle: character to check
+ * Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *start = needle;
+
+
+	for ( ; *haystack != '\0'; haystack++)
+	{
+		if (*haystack == *needle)
+		{
+			needle = start;
+			for ( ; *needle != '\0'; needle++, haystack++)
+			{
+				if (*needle == *haystack)
+				{
+					return (needle);
+				}
+				if (needle == '\0')
+				{
+					break;
+				}
+			}
+		}
+	}
+	return ('\0');
+}

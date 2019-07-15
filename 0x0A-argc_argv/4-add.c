9acd4e54d9b12bf6b1eadc0b3a8450ec21b43e5c
Banu Sapakova <banuaksom@gmail.com>
2019-07-15 06:38:54 +0000

    took null condition out of a loop

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,21 +8,27 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j;
-	unsigned int result = 0;
+	int i, j, result = 0;
 
-	for (i = 1; i < argc; i++)
+	if (argv[1] == '\0')
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
-			{
-				puts("Error");
-				return (1);
-			}
-		}
-		result += atoi(argv[i]);
+		printf("%d\n", result);
 	}
-	printf("%d\n", result);
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			result += atoi(argv[i]);
+		}
+		printf("%d\n", result);
+	}
 	return (0);
 }

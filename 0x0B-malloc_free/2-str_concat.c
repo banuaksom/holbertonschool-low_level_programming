0a0318246ba9748e8e6d2d1585162cc88e31ab57
Banu Sapakova <banuaksom@gmail.com>
2019-07-16 23:06:21 +0000

    added

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,33 +14,22 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0;
 	int len2 = 0;
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		*s1 = "";
-		*s2 = "";
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		len1++;
-	}
 	for (i = 0; s2[i] != '\0'; i++)
-	{
 		len2++;
-	}
 	new_str = malloc((len1 + len2) * sizeof(char) + 1);
 	if (new_str == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < len1; i++)
-	{
 		new_str[i] = s1[i];
-	}
 	for (i = len1, j = 0; i < len1 + len2 && j < len2; i++, j++)
-	{
 		new_str[i] = s2[j];
-	}
 	new_str[i] = '\0';
 	return (new_str);
 	free(new_str);

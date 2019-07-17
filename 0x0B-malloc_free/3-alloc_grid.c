1c95ada849e0aaf3be0d3864095f9b3233cdbc53
Banu Sapakova <banuaksom@gmail.com>
2019-07-17 03:10:17 +0000

    added sizeof *

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,22 +15,13 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = malloc(height * sizeof(int));
+	grid = malloc(height * sizeof(int *));
 
 	if (grid == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(width * sizeof(int));
-		if (grid[i] == NULL)
-		{
-			for ( ; i >= 0; i--)
-			{
-				free(grid[i]);
-			}
-			free(grid);
-			return (NULL);
-		}
 		for (j = 0; j < width; j++)
 		{
 			*(*(grid + i) + j) = 0;

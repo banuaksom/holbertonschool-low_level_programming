8fda00e9342c09552525029535714f71764cb62d
Banu Sapakova <banuaksom@gmail.com>
2019-07-18 23:09:26 +0000

    added

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * _realloc - reallocates a memory block
+  * @ptr: character to check
+  * @old_size: character to check
+  * @new_size: character to check
+  * Return: void
+  */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	if (ptr == NULL)
+		return (p);
+	for (i = old_size; i < new_size; i++)
+	{
+		*(p + i) = *((char *)ptr + i);
+	}
+	free(ptr);
+	return (p);
+}

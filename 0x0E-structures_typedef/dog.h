796329a61a83685b8e6261d5573bfd3775096fa9
Banu Sapakova <banuaksom@gmail.com>
2019-07-22 21:01:36 +0000

    first add

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+/**
+  * struct dog - struct named dog
+  * @name: struct element
+  * @age: struct element
+  * @owner: struct element
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

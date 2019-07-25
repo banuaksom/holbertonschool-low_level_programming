d6e93eac34eb73846a6388d26cb29c0c16916649
Banu Sapakova <banuaksom@gmail.com>
2019-07-25 04:00:09 +0000

    third add

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,6 +2,7 @@
 #define CALC_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * struct op - Struct op
  *
@@ -19,4 +20,4 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-#endif
+#endif /* CALC_H */

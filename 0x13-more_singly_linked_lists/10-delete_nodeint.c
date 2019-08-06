c3354736f5aee29738947181bc65e065d4e34b19
Banu Sapakova <banuaksom@gmail.com>
2019-08-06 22:59:09 +0000

    changed back

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,6 +15,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	for (i = 0; *head && index; i++)
 	{
+		if (!swap)
+			return (-1);
 		if (i == index - 1)
 			break;
 		swap = swap->next;

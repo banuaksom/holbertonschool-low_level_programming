27904221d478192801c1e193228c809cd4dd1f20
Banu Sapakova <banuaksom@gmail.com>
2019-08-13 00:37:18 +0000

    added first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+  * myStartupFun - prints the string before main function
+  */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

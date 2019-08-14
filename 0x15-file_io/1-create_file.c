c125bc69db31244a3752abcd73dadfe79375f8a8
Banu Sapakova <banuaksom@gmail.com>
2019-08-14 02:57:10 +0000

    added

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	w = write(fd, text_content, _strlen(text_content));
-	if (w == -1 || _strlen(text_content) == -1)
+	if (w == -1)
 		return (-1);
 	close(fd);
 	return (1);

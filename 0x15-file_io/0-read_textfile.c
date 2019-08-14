d28bb35abfd279e76fce37471836f7457ad2eebd
Banu Sapakova <banuaksom@gmail.com>
2019-08-14 01:53:21 +0000

    first added

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+  * read_textfile - reads a text file and prints it to the POSIX standard output
+  * @filename: pointer to the text
+  * @letters: number of letters to read and print
+  * Return: int
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = open(filename, O_RDONLY);
+	char *buf = malloc(sizeof(char) * letters + 1);
+	int l;
+
+	if (fd == -1 || !filename || !buf)
+		return (0);
+	l = read(fd, buf, letters);
+	if (l == -1)
+		return (0);
+	buf[l] = '\0';
+	l = write(STDOUT_FILENO, buf, l);
+	if (l == -1)
+		return (0);
+	close(fd);
+	free(buf);
+	return (l);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ * Return: the length of a string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+	return (i);
+}
+/**
+ * create_file - creates a file
+ * @filename: name of a file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		return (1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	w = write(fd, text_content, _strlen(text_content));
+	if (w == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}

bd3c6efe71cb96bdf51726ec482a07e534c3aa93
Banu Sapakova <banuaksom@gmail.com>
2019-08-14 04:38:33 +0000

    added

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of args
+ * @argv: filenames
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int fd1, fd2, l;
+	char buf[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	l = read(fd1, buf, 1024);
+	if (l > 0)
+	{
+		if ((write(fd2, buf, l)) != l || fd2 == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	if (l == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(fd1) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
+	if (close(fd2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
+	return (0);
+}

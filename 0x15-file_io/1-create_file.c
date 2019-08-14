44c4c937a9ef08700a49b14bf1a97619ef5b0909
Banu Sapakova <banuaksom@gmail.com>
2019-08-14 02:48:39 +0000

    added return for string length

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	w = write(fd, text_content, _strlen(text_content));
-	if (w == -1 || w != _strlen(text_content))
+	if (_strlen(text_content) == -1)
 		return (-1);
 	close(fd);
 	return (1);

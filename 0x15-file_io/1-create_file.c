5f38daa0347ceeabb7c1c26c02666d36de4b8286
Banu Sapakova <banuaksom@gmail.com>
2019-08-14 02:32:05 +0000

    added return for compare result

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int _strlen(char *s)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w;
+	int fd;
 
 	if (!filename)
 		return (-1);
@@ -32,8 +32,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	w = write(fd, text_content, _strlen(text_content));
-	if (w == -1)
+	write(fd, text_content, _strlen(text_content));
+	if (_strlen(text_content) != write(fd, text_content, _strlen(text_content)))
 		return (-1);
 	close(fd);
 	return (1);

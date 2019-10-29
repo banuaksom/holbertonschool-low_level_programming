61887db18d9f1f3a63adcf5a12d1c760d9cb233c
Banu Sapakova <banuaksom@gmail.com>
2019-10-29 03:45:14 +0000

    added

---
+++ 0x1B-sorting_algorithms/sort.h
@@ -0,0 +1,30 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+void print_array(const int *array, size_t size);
+void swap(int *a, int *b);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void delete_node(listint_t *node);
+void counting_sort(int *array, size_t size);
+int max_arr(int *array, size_t size);
+void count_sort(int *array, int size, int diviser);
+void radix_sort(int *array, size_t size);
+#endif
